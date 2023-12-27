0b22e8f1ed59976bb664cbf8ce0c90a39d66b5c7
alex-boudreaux <43324872+AlexBoudreaux@users.noreply.github.com>
2023-12-27 15:16:45 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React NOW BRO!
+          Learn React
         </a>
       </header>
     </div>
